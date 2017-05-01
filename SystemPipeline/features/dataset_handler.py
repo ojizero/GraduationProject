70b78be86edd09aa03baf9a35de45e22054d8e5a
ojizero <z.acnts.816@protonmail.ch>
2017-05-01 09:55:24 +0300

    feature vector maker

---
+++ SystemPipeline/features/dataset_handler.py
@@ -0,0 +1,3 @@
+import sys
+# Add project to path, to be able to import modules from it
+sys.path.append('/Users/oji/Workspace/Self/GraduationProject/SystemPipeline')
---
+++ SystemPipeline/features/features_transformer.py
@@ -0,0 +1,7 @@
+import sys
+# Add project to path, to be able to import modules from it
+sys.path.append('/Users/oji/Workspace/Self/GraduationProject/SystemPipeline')
+
+'''
+produces a feature vector from the result of feature extraction
+'''
