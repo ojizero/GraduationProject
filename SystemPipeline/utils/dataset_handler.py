ecec8e468c41523b4cf440cbea62cd047fde74b2
ojizero <z.acnts.816@protonmail.ch>
2017-05-08 00:35:26 +0300

    inital converter

--- SystemPipeline/utils/dataset_handler.py
+++ SystemPipeline/utils/dataset_handler.py
@@ -18,10 +18,13 @@ _verbal_code = re.compile(r'\s+|array|\(|\)|\'|"')
 _path_label_pattern = r'^.*/(\w*)(?:\.\w+)?/.*\.csv$'
 _rgx = re.compile(_path_label_pattern)
 
+_array_regex = re.compile(r'^\[.*\]$')
+_float_regex = re.compile(r'^\d*(?:\.\d+)?$')
+_cmplx_regex = re.compile(r'^.*[+-]\d+(?:\.\d+)?j$')
+
 
 class DatasetHandler:
-	_LABEL_MAKER = lambda path: _rgx.search(path).groups()[0]
-	_FEATURE_VECTOR_MAKER = FeaturesTransformer.transform
+	_FEATURE_VECTOR_EXTRACTOR = FeaturesTransformer.transform
 
 	def __init__ (self, **kwargs):
 		self.vector_names     = kwargs.pop('vector_names', ())
@@ -32,8 +35,8 @@ class DatasetHandler:
 	@classmethod
 	def from_csv_directory (cls, path, **kwargs):
 		files_iterator = iglob('%s/**/**/*.csv' % path)
-		vector_maker   = kwargs.pop('vector_maker', cls._FEATURE_VECTOR_MAKER)
-		label_maker    = kwargs.pop('label_maker', cls._LABEL_MAKER)
+		vector_maker   = kwargs.pop('vector_maker', cls._FEATURE_VECTOR_EXTRACTOR)
+		label_maker    = kwargs.pop('label_maker', lambda path: _rgx.search(path).groups()[0])
 		delimiter      = kwargs.pop('delimiter', ',')
 
 		def _dataset_generator ():
@@ -54,8 +57,10 @@ class DatasetHandler:
 		vector_maker = lambda row: (row[0], vector_names, row[1:])
 
 		def _dataset_generator ():
-			for row in np.genfromtxt(input_generator, delimiter=delimiter, dtype=np.float):
+			for row in np.genfromtxt(input_generator, delimiter=delimiter, dtype=np.complex):
 				yield vector_maker(row)
+			# for row in input_generator:
+			# 	yield vector_maker(cls._convert_str(row, delimiter))
 
 		return cls(dataset_iterator=_dataset_generator(), vector_names=vector_names, **kwargs)
 
@@ -100,6 +105,38 @@ class DatasetHandler:
 			for line in in_:
 				yield line
 
+	@staticmethod
+	def _convert_str (string, delimiter):
+		# return string
+		if isinstance(string, bytes):
+			string = string.decode()
+
+		is_array = lambda s: _array_regex.match(s)
+		is_float = lambda s: _float_regex.match(s)
+		is_cmplx = lambda s: _cmplx_regex.match(s)
+
+		def _converter ():
+			cols = string.split(delimiter)
+
+			for col in cols:
+				ret = None
+
+				if is_array(col):
+					ret = DatasetHandler._convert_str(col[1:-1], ',')
+				elif is_float(col):
+					ret = np.float(col)
+				elif is_cmplx(col):
+					ret = np.complex(col)
+				else:
+					print(type(col), col)
+					raise Exception('unhandled datatype')
+
+				yield ret
+
+		return np.array([*_converter()])
+
+
+
 
 if __name__ == '__main__':
 	# dataset = DatasetHandler.from_csv_directory(path='/Users/oji/Workspace/Self/GraduationProject/SystemPipeline/data')
@@ -107,5 +144,5 @@ if __name__ == '__main__':
 	# dataset.store_csv('/Users/oji/Workspace/Self/GraduationProject/SystemPipeline/dataset_dump.csv')
 	dataset = DatasetHandler.from_csv_file('/Users/oji/Workspace/Self/GraduationProject/SystemPipeline/dataset_sample.csv')
 
-	for d in dataset.dataset_iterator:
-		print(type(d))
+	for d in dataset:
+		print(d)
