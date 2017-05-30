13c71fe931209a5d26d133406df901e55b06c679
ojizero <z.acnts.816@protonmail.ch>
2017-05-31 00:02:31 +0300

    checking tensorflow

---
+++ SystemPipeline/learners/cnn_tf.py
@@ -0,0 +1,16 @@
+import sys
+# Add project to path, to be able to import modules from it
+sys.path.append('/Users/oji/Workspace/Self/GraduationProject/SystemPipeline')
+
+import numpy as np
+import tensorflow as tf
+
+# data structuring
+# CNN handles images, we don't have images
+
+# one solution is to restructure the data as an image
+# # two paths for restructuring while preserving correlations
+# # # -> height, width = streams, features per stream
+# # # -> height, width = streams*windows, features per window
+
+# no need for feature selection here, let and let be
