6e61f097e94e35ac2b08afddbe0d6b73fad4880a
ojizero <z.acnts.816@protonmail.ch>
2017-06-01 22:56:15 +0300

    restrucutre implementaion, not tested yet

--- SystemPipeline/features/restructurer.py
+++ SystemPipeline/features/restructurer.py
@@ -2,12 +2,27 @@ import sys
 # Add project to path, to be able to import modules from it
 sys.path.append('/Users/oji/Workspace/Self/GraduationProject/SystemPipeline')
 
+import numpy as np
 
 from utils.decorators import classinstancemethod
 from features.features_transformer import FeaturesTransformer
 
 class StructureTransformer (FeaturesTransformer):
+	'''
+	Restructures the feature vector in an image like format
+	'''
 	@classinstancemethod
 	def transform (obj, **kwargs):
 		names, values = super().transform(**kwargs)
-		# do transfomation here
+		names, values = np.array(names), np.array(values)
+
+		windows_count = kwargs.get('windows_count', obj._windows_count)
+		streams_count = kwargs.get('streams_count', 6)
+
+		step = len(names) // streams_count // windows_count
+		end  = len(names)
+
+		names  = np.array([names[pivot:pivot+step]  for pivot in range(0, end, step)])
+		values = np.array([values[pivot:pivot+step] for pivot in range(0, end, step)])
+
+		return names, values, values.shape
